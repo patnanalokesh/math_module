8271a33a996ad3b2ef5179e234f9ce015d58c646
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-08-24 17:55:33 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,134 @@
+# import math 
+# in case of we didn't import 'math' module then we got an error is called as "NameError: name 'math' is not defined"
+
+# print(dir(math))
+
+# print(math.lcm(45,24))
+# print(math.lcm(12,17))
+# print(math.lcm(11,19,25))
+# print(math.lcm(8,12))
+
+# print(math.gcd(24, 32))
+# print(math.gcd(28, 48))
+# print(sum(1,2,3,4,5,6))  # TypeError: sum() takes at most 2 arguments (6 given)
+# print(sum(1,2))  # TypeError: 'int' object is not iterable
+
+# a=1,2,3,4,5
+# print(sum(a))
+# import math
+# print(math.prod(a))
+
+# import math
+# print(math.factorial(2))
+# print(math.factorial(4))
+# print(math.factorial(7))
+
+# print(abs(-65))  # here 'abs' means negative value is converting into positive value.
+# print(abs(-44+3))
+
+# import math
+# print(math.fabs(-44+8))  # here 'fabs' giving the value as like'abs' but in the method of 'float'.
+# print(math.fabs(-87+65))
+# print(math.fabs(-55+97))
+
+# a=1,5,6,8
+# print(sum(a))
+# print(math.fsum(a))  # when we are not using math module then we got an error like "NameError: name 'math' is not defined"
+# here 'fabs' giving the value as like'abs' but in the method of 'float'.
+
+# import math
+# print(math.factorial(0))
+# print(math.factorial(1))
+
+import math 
+# print(math.ceil(0.1))
+# print(math.ceil(0.5))
+# print(math.ceil(1.5))
+# print(math.ceil(3.6))
+# print(math.ceil(55.45))  # if here the value in positive the next number will executed.
+# print(math.ceil(-0.9))  # here there is no "-o" either in mathematics or else anywhere. so, here only zero will be executed.
+# print(math.ceil(-1.8))   # if here the value in negative the original number will executed and eliminates all the point values.
+# print(math.ceil(-7.80))
+# print(math.ceil(-4.3))
+# print(math.ceil(22.4548))
+
+# print(math.exp(1))
+# print(math.exp(5))
+
+# print(math.factorial(0))
+# print(math.factorial(4))  # here factorial means like in mathematics : 3!=3*2*1=6
+# print(math.factorial(5))
+# print(math.factorial(6))
+# print(math.factorial(10))
+
+# print(math.floor(5.6))
+# print(math.floor(25.15))    # here the positive is have.so, the original number will executed and the remaining point values could be elimiated.
+# print(math.floor(45.15578))
+# print(math.floor(89.01248))
+# print(math.floor(-65.1245))  # here the negative is have.so, the next number will be executed.
+# print(math.floor(-5.3))
+# print(math.floor(-48.1246458))
+
+# a=math.pow(4, 3)
+# b=3**3
+# print(a,type(a))
+# print(b,type(b))
+# print(math.pi)
+# print(math.pi*2)
+# print(math.pi*4**3)    # here we use mathematical fomulae which like "pir2".
+
+# print(math.sqrt(4))
+# print(math.sqrt(8.4))
+# print(math.sqrt(25))
+# print(math.sqrt(16))  # here give the exact value when we use "sqrt"
+# print(math.sqrt(18))
+# print(math.sqrt(32))
+# print(math.sqrt(72))
+# c=2   # 1 to 3 will give 1 if used "isqrt"
+# d=6  # 4 to 8 will give 2 if used "isqrt"
+# w=14 # 9 to 15 will give 3 if used "isqrt"
+# f=0  # 0 will give 0 if used "isqrt"
+# g=30  # 25 to 35 will give 5 if used "isqrt"
+# print(math.isqrt(c))
+# print(math.isqrt(d))
+# print(math.isqrt(w))
+# print(math.isqrt(f))
+# print(math.isqrt(g))
+
+# b=[1,2,3,4,5,6]
+# print(sum(b))
+# print(math.fsum(b))
+# print(math.prod(b))
+# print(int(math.fsum(b)))
+
+# f=17%3
+# print(f)
+# k=55%5
+# print(k)  #  here the answer will come by the division of remainder value.
+# d=45841%254
+# print(d)
+# print(math.fmod(23, 8))
+# print(math.fmod(23, 3))
+
+# v=-55
+# print(abs(v))
+# a=abs(int(input()))
+# print('The person age is',a)
+
+# print(math.fabs(-88))
+# print(math.cos(45))
+# print(math.cos(30))
+# print(math.sin(45))
+# print(math.sin(30))
+# print(math.sin(90))
+# print(math.tan(45))
+
+# trucate
+# print(math.trunc(45.23615))
+# print(math.trunc(-65.1245))  # here, whether the value is in either positive or negative then original value will executed and the point value will be eliminated or trucated. 
+# print(int(-65.12245))
+
+# print(math.lcm(45,21,87))   # LCM : Least Common Factor
+# print(math.gcd(45,254, 360))  # GCD : Greatest Common Divisor
+# print(math.lcm(23,32))
+# print(math.gcd(23, 32))
